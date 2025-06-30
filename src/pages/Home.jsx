dc14b03c803ca754f899ef0a8b0769ece9f96a3b
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-06-30 20:45:36 +0530

    added navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { NavBar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -10,7 +11,7 @@ export const Home = () => {
     
       <StarBackground/>
       {/* Navbar */}
-    
+       <NavBar/>
       {/* Main Content */}
       
 
