772936dded4c33ec8c07f7a4b018ba68ce525483
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-01 18:06:24 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+export const Footer = () => {
+  return (
+    <footer className="py-5 relative">
+     
+      <p className="text-sm text-muted-foreground">
+        &copy; {new Date().getFullYear()} Manish Verma. All rights reserved.
+      </p>
+    </footer>
+  );
+};
