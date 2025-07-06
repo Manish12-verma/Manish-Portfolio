2263100a75442d1d6b826b31457150963c9d5be6
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-06 09:00:46 +0530

    added hireMe button in HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,11 +12,14 @@ export const HeroSection = () => {
                 <p className="text-lg md:text-xl text-muted-foreground max-w-4xl mx-auto opacity-0  animate-fade-in-delay-3">
                     Passionate Software Developer crafting intuitive digital experiences.
                    Specializing in frontend development, skilled in building robust web applications with clean, scalable code.</p>
+             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
              
-             <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-                 <a href="#projects" className="cosmic-button">
+                 <a href="#projects" className="cosmic-button opacity-0 animate-fade-in-delay-4">
                 View my work
              </a>
+              <a href="https://www.linkedin.com/in/manish-verma-986033233/" className=" opacity-0 animate-fade-in-delay-4 px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                      Hire Me
+                  </a>
              </div>
             </div>
       </div>
